eecf7119d13533cb9d32badc0df7be11e1b79ce9
tgoddessana <twicegoddessana1229@gmail.com>
2024-03-24 23:43:08 +0900

    feat: migrate to django-ninja, from DRF

--- config/ninja_api.py
+++ config/ninja_api.py
@@ -1,8 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from ninja import NinjaAPI
 
 from cookiecutter_django_ninja_example.users.api.views import users_router
 
 # Overrides to maintain consistency with the DRF url namespace in cookiecutter-django.
-api = NinjaAPI(urls_namespace="api")
+api = NinjaAPI(urls_namespace="api", docs_decorator=staff_member_required)
 
 api.add_router("/users/", users_router)
--- cookiecutter_django_ninja_example/users/api/views.py
+++ cookiecutter_django_ninja_example/users/api/views.py
@@ -1,26 +1,58 @@
-from rest_framework import status
-from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin
-from rest_framework.mixins import RetrieveModelMixin
-from rest_framework.mixins import UpdateModelMixin
-from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from ninja import Router
+from ninja.errors import HttpError
 
 from cookiecutter_django_ninja_example.users.models import User
 
-from .serializers import UserSerializer
+from .schemas import UserSchema
+from .schemas import UserUpdateSchema
+
+users_router = Router(tags=["users"])
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-    lookup_field = "username"
+# error handling in django-ninja is different from DRF
+# see https://django-ninja.dev/guides/errors/
+# do we need to add a custom error handler function in template?
 
-    def get_queryset(self, *args, **kwargs):
-        assert isinstance(self.request.user.id, int)
-        return self.queryset.filter(id=self.request.user.id)
 
-    @action(detail=False)
-    def me(self, request):
-        serializer = UserSerializer(request.user, context={"request": request})
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+@users_router.get("me/", response=UserSchema, url_name="user-me")
+def user_me(request):
+    return request.user
+
+
+@users_router.get("/", response=list[UserSchema], url_name="user-list")
+def user_list(request):
+    return User.objects.all()
+
+
+@users_router.get("{username}/", response=UserSchema, url_name="user-detail")
+def user_detail(request, username: str):
+    try:
+        return User.objects.get(username=username)
+    except User.DoesNotExist as exc:
+        raise HttpError(404, "User not found") from exc
+
+
+@users_router.put("{username}/", response=UserSchema, url_name="user-update")
+def update_user(request, username: str, payload: UserUpdateSchema):
+    try:
+        user = User.objects.get(username=username)
+        user.username = payload.username
+        user.name = payload.name
+        user.save()
+    except User.DoesNotExist as exc:
+        raise HttpError(404, "User not found") from exc
+
+    return user
+
+
+@users_router.patch("{username}/", response=UserSchema, url_name="user-partial-update")
+def partial_update_user(request, username: str, payload: UserUpdateSchema):
+    try:
+        user = User.objects.get(username=username)
+        user.username = payload.username
+        user.name = payload.name
+        user.save()
+    except User.DoesNotExist as exc:
+        raise HttpError(404, "User not found") from exc
+
+    return user
